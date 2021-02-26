7cf1abba0fd6b7ee379e0d00100d6f38b11ca3af
Maciej Pie <maciejpiech18@gmail.com>
2021-02-26 11:30:49 -0600

    updated db

--- db/db.js
+++ db/db.js
@@ -1,5 +1,7 @@
 const { Sequelize, STRING } = require('sequelize');
-const db = new Sequelize('postgres://localhost/dealers_choice');
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/dealers_choice'
+);
 
 const Glove = db.define('glove', {
   name: {
